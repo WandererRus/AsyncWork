2e60eb0c9b64fa70a815a02281bcf1f7d82052b1
Fastov Ilya Olegovich <iluxailuxa@gmail.com>
2023-02-20 20:45:08 +0300

    123

--- AsyncWork/Program.cs
+++ AsyncWork/Program.cs
@@ -6,9 +6,11 @@
         {
             WaitCallback callback = new WaitCallback(Sum);
 
-            for (int i = 1; i <= 20; i++) 
-            { 
-                ThreadPool.QueueUserWorkItem(callback,i * 100);
+
+
+            for (int i = 1; i <= 20; i++)   
+            {                
+                ThreadPool.QueueUserWorkItem(callback,i * 10000);
             }
 
             Console.WriteLine("Hello, World!");
@@ -21,7 +23,7 @@
             int result = 0;
             for (int i = 0; i < (int)count; i++)
             {
-                result+= i;                
+                result += i;                
             }
             Console.WriteLine("Номер потока: " + Thread.CurrentThread.ManagedThreadId);
             Console.WriteLine(result);
