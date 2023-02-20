745b67a7bf40c10f1b06b5778648afcc6903986b
Fastov Ilya Olegovich <iluxailuxa@gmail.com>
2023-02-20 20:35:57 +0300

    Добавьте файлы проекта.

---
+++ AsyncWork/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace AsyncWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WaitCallback callback = new WaitCallback(Sum);
+
+            for (int i = 1; i <= 20; i++) 
+            { 
+                ThreadPool.QueueUserWorkItem(callback,i * 100);
+            }
+
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+
+        static void Sum (object count) 
+        {
+
+            int result = 0;
+            for (int i = 0; i < (int)count; i++)
+            {
+                result+= i;                
+            }
+            Console.WriteLine("Номер потока: " + Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine(result);
+        }
+
+
+
+    }
+}
